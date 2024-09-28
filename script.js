9aff60284be340987ef7afa9674daa7937f27e8c
Rahul <rahultime2018@gmail.com>
2024-09-28 12:08:05 +0530

    .

--- script.js
+++ script.js
@@ -14,21 +14,3 @@ closeMenuButton.addEventListener("click", function(){
 const body = document.querySelector("body");
 const eventsPage = document.getElementById("eventsPage");
 
-window.addEventListener("scroll", () => {
-    const eventsPagePosition = eventsPage.getBoundingClientRect().top;
-    const windowHeight = window.innerHeight;
-
-    if (eventsPagePosition <= windowHeight * 0.5) {
-        body.classList.add("fade-out"); // Start fade-out effect
-        setTimeout(() => {
-            body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.801), rgba(0, 0, 0, 0.8)), url('3d.jpg')";
-            body.classList.remove("fade-out"); // Remove fade-out effect after image change
-        }, 500); // Match this with the CSS transition duration
-    } else {
-        body.classList.add("fade-out"); // Start fade-out effect
-        setTimeout(() => {
-            body.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.801), rgba(0, 0, 0, 0.8)), url('website-background.jpg')";
-            body.classList.remove("fade-out"); // Remove fade-out effect after image change
-        }, 500); // Match this with the CSS transition duration
-    }
-});
