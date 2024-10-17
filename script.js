e135051da9a3bbfa28e86e77213818b6125cf392
Rahul Raj <rahultime2018@gmail.com>
2024-10-18 01:12:51 +0530

    .

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const menuButton = document.getElementById("openMenu")
-const sidebar = document.querySelector(".sidebar")
-menuButton.addEventListener('click', function(){
-    sidebar.style.visibility = "visible"
-    menuButton.style.visibility = "hidden"
-    sidebar.style.left = "0px";
-})
+const menuButton = document.getElementById("openMenu");
+const sidebar = document.querySelector(".sidebar");
+menuButton.addEventListener("click", function () {
+  sidebar.style.visibility = "visible";
+  menuButton.style.visibility = "hidden";
+  sidebar.style.left = "0px";
+});
 
-const closeMenuButton = document.getElementById("closeMenu")
-closeMenuButton.addEventListener("click", function(){
-    sidebar.style.visibility = "hidden"
-    menuButton.style.visibility = "visible"
-    sidebar.style.left = "-100vw"
-})
+const closeMenuButton = document.getElementById("closeMenu");
+closeMenuButton.addEventListener("click", function () {
+  sidebar.style.visibility = "hidden";
+  menuButton.style.visibility = "visible";
+  sidebar.style.left = "-100vw";
+});
 
 const body = document.querySelector("body");
 const eventsPage = document.getElementById("eventsPage");
-
